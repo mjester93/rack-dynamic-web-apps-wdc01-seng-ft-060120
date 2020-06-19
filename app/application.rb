1d017542bc9f89ff283470b3556b36575feffb9d
Michael Jester <m.jester.93@gmail.com>
2020-06-19 02:50:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    num1 = Kernal.rand(1..20)
-    num2 = Kernal.rand(1..20)
-    num3 = Kernal.rand(1..20)
+    num1 = Kernel.rand(1..20)
+    num2 = Kernel.rand(1..20)
+    num3 = Kernel.rand(1..20)
 
     if num1 == num2 && num2 == num3
       resp.write "You Win"
